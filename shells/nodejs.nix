f9c1c3a7710c5c802f3c56a80962af807eddc781
Cecilia Sanare <ceci@sanare.dev>
2024-03-26 11:54:36 -0500

    feat: added support fo reading nvmrc files

--- shells/nodejs.nix
+++ shells/nodejs.nix
@@ -1,7 +1,8 @@
 { lib, pkgs ? (import <nixpkgs>) { }, version ? 20 }: 
 let
   # Adds support for formats like 20.11.1
-  version_short = builtins.elemAt (builtins.split "\\." (toString version)) 0;
+  version_resolved = if builtins.isPath(version) then builtins.readFile(version) else version;
+  version_short = builtins.elemAt (builtins.split "\\." (toString version_resolved)) 0;
   nodejs = pkgs.${"nodejs_" + version_short};
 in
 pkgs.mkShell {
